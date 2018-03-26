db93316b96bad67b512dd9fe80c71177bb046e3b
Maxim Zaverukha <m.zaverukha@agilites.com>
2018-03-26 17:18:41 +0300

    Fixed comments for exceptions

--- src/main/java/com/ufo/core/exception/NotSupportedYetException.java
+++ src/main/java/com/ufo/core/exception/NotSupportedYetException.java
@@ -5,10 +5,23 @@ package com.ufo.core.exception;
  * <p>
  * Runtime exception to inform about not supported functionality
  */
+@SuppressWarnings("unused")
 public class NotSupportedYetException extends RuntimeException {
 
     public NotSupportedYetException(String message) {
         super(message);
+    }
+
+    public NotSupportedYetException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotSupportedYetException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotSupportedYetException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 
 }
